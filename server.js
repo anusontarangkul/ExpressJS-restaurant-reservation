b9dc7cf75e569f30b9bedc46e80d4e32508d2ae6
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-10-18 20:15:32 -0700

    add html routes

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const PORT = process.env.PORT || 8000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+
+// require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
 });
